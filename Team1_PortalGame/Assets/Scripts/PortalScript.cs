30d4c4c4162f096b81469e766f261247100f2a75
Kristen Holcombe <infinitestorylines@gmail.com>
2025-02-27 19:08:54 -0500

    Worked on Portals in KristenScene.
    Created a basic portal script and started creating a portal prefab whose target is set in trigger box.

---
+++ Team1_PortalGame/Assets/Scripts/PortalScript.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour
+{
+
+    //set target in the Portal's trigger box. 
+
+    [SerializeField]
+    private Transform target;
+
+    private void Start()
+    {
+        if(target == null)
+        {
+            print("set target in inspector of Portal trigger box!");
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        print("entered collider");
+        if(other.gameObject.CompareTag("Player"))
+        {
+            print("player entered collider");
+
+            print("target.transform.position is " + target.transform.position);
+
+            other.transform.position = target.transform.position;
+
+           print("position changed");
+        }
+    }
+}
