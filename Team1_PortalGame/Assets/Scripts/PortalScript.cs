68832c441fce7e16b47a1c57da5056d6b1f193c8
Kristen Holcombe <infinitestorylines@gmail.com>
2025-03-02 19:50:02 -0500

    Portals work now
    Added Physics.SyncTransforms() line to PortalScript.

--- Team1_PortalGame/Assets/Scripts/PortalScript.cs
+++ Team1_PortalGame/Assets/Scripts/PortalScript.cs
@@ -21,13 +21,15 @@ public class PortalScript : MonoBehaviour
         print("entered collider");
         if(other.gameObject.CompareTag("Player"))
         {
-            print("player entered collider");
+            //print("player entered collider");
+            //print("target.transform.position is " + target.transform.position);
 
-            print("target.transform.position is " + target.transform.position);
+            
 
             other.transform.position = target.transform.position;
+            Physics.SyncTransforms();
 
-           print("position changed");
+           //print("position changed");
         }
     }
 }
