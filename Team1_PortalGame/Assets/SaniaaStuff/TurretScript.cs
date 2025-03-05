e3afaf963e8c812cd7ef2679b88bee3202bf8f5e
Saniaabosley <77111639+Saniaabosley@users.noreply.github.com>
2025-03-05 14:55:43 -0500

    turret script

--- Team1_PortalGame/Assets/SaniaaStuff/TurretScript.cs
+++ Team1_PortalGame/Assets/SaniaaStuff/TurretScript.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class TurretScript : MonoBehaviour
 {
+   ///publics
 
     public GameObject bulletPrefab;
     public bool attackMode;
     public bool idleMode;
+    public Transform bulletSpawnPoint; //where the bullet will come from 
+    public float fireRate = 1f; //how fast the turret will shoot 
+    
+    //privates
+
+    private float fireIntervals = 0f; // time inbetween each shot
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -36,9 +43,29 @@ public class TurretScript : MonoBehaviour
         {
             Debug.Log("Turrets are firing at the player");
             idleMode = false; 
+            Fire();
         }
 
+    
+    }
+
+     void Fire()
+    {
+        if (Time.time >= nextFireTime)
+        {
+           
+            Shoot();
+
+            nextFireTime = Time.time + fireRate;
+        }
+    }
+
+     void Shoot()
+    {
         
-  
+        if (bulletPrefab && bulletSpawnPoint)
+        {
+            Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        }
     }
 }
