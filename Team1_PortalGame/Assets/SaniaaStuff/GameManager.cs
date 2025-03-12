1f2aaa29dea0c53b0e69db4d5cde5a6371655b76
Saniaabosley <77111639+Saniaabosley@users.noreply.github.com>
2025-03-12 15:56:08 -0400

    gamemanager stuff

--- Team1_PortalGame/Assets/SaniaaStuff/GameManager.cs
+++ Team1_PortalGame/Assets/SaniaaStuff/GameManager.cs
@@ -1,14 +1,37 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
 
     public float playerHealth = 100;
+    public Image redFlashImage; //flash image for when the player gets hit
+    public float flashDuration = 0.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        redFlashImage.enabled = false;
+         //redFlashImage = GameObject.Find("RedFlash").GetComponent<Image>();
+    }
+
+    void OnTriggerEnter(Collider other)
+{
+    if (other.CompareTag("Bullet"))
+    {
+        playerHealth -= 15;
+        Debug.Log("Player hit " + playerHealth);
+        StartCoroutine(FlashRedScreen());
+    }
+}
+
+private IEnumerator FlashRedScreen()
+    {
         
+        redFlashImage.enabled = true;
+        yield return new WaitForSeconds(flashDuration);
+        redFlashImage.enabled = false;
     }
 
     // Update is called once per frame
